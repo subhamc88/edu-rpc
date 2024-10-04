b0cb714b50202b2fe46448cceb9efd746b87e2b6
subhamc88 <subham020304@gmail.com>
2024-10-05 01:14:34 +0530

    updated cliend.py for taking user input and generate a request

--- python/client.py
+++ python/client.py
@@ -28,3 +28,36 @@ if __name__ == "__main__":
     print("Multiplying 10 and 10:", send_request("multiply", 10, 10))
     print("Dividing 10 and 10:", send_request("quotient", 10, 10))
     print("Finding Remainder between 10 and 10:", send_request("remainder", 10, 10))
+
+
+# Taking your request
+    method = "add"
+    number_1 = 1
+    number_2 = 1
+
+    # Only accepts string as input.
+    while True:
+        user_input_method = input("Enter your method/operation (Only String Allowed): \nadd\nsub\nmultiply\nquotient\nremainder\n")
+        if user_input_method in ["add", "sub", "multiply", "quotient", "remainder"]:
+            method = user_input_method
+            break
+
+    # Only accepts number as input.
+    while True:
+        user_number_1 = input("Enter your first number (Only Number Allowed): ")
+        try:
+            number_1 = float(user_number_1)
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+    # Only accepts number as input.
+    while True:
+        user_number_2 = input("Enter your second number (Only Number Allowed): ")
+        try:
+            number_2 = float(user_number_2)
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+    print(f"Response from server: {send_request(method, number_1, number_2)}")
